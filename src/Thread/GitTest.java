5ef7bb9eaf493c6d6ffc8696fe19152e6fe6e2ff
wuqinyuan <wqy16363@163.com>
2023-04-21 17:01:31 +0800

    分支添加了一行

---
+++ src/Thread/GitTest.java
@@ -0,0 +1,11 @@
+package Thread;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git1");
+        System.out.println("git2");
+        System.out.println("git3");
+        System.out.println("git4");
+
+    }
+}
